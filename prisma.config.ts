9173d4bfb827db42012ff88641451c3190e770ad
jrFrancesco <francesco@hcunit.com.br>
2026-07-20 19:37:15 -0300

    Update database URL environment variable

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,8 +2,7 @@ import 'dotenv/config';
 import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
   datasource: {
-    url: env('DIRECT_DATABASE_URL'),
+    url: env('DATABASE_URL'),
   },
 });
